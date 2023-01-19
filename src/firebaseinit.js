9b896aa1a62510dbc2097e23635373fba88aad70
ashinsabu <ashin.sabu3@gmail.com>
2023-01-19 14:16:37 +0530

    Firebase support and initial UI with header

---
+++ src/firebaseinit.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDuZLVk6B1xG-zN_dQVOlGa5ayT5nSW2nU",
+    authDomain: "image-seq-password.firebaseapp.com",
+    projectId: "image-seq-password",
+    storageBucket: "image-seq-password.appspot.com",
+    messagingSenderId: "1012026876474",
+    appId: "1:1012026876474:web:564aec9752b422cb604b03",
+    measurementId: "G-CVSCK6B1PL"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
